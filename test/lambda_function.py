3962c5a00a099df635108ac0454945d8a3faebde
hisato-kawaji <hkawaji@moff.mobi>
2017-07-28 17:24:30 +0900

    change directory

---
+++ test/lambda_function.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from pandas import DataFrame
+# test directory
+
+def lambda_handler(event, context):
+    # TODO implement
+    df1 = DataFrame([{'a': 1, 'b': 1, 'c': 1}, {'a': 2, 'b': 1, 'c': 3}])
+    df2 = DataFrame([{'a': 1, 'b': 1, 'c': 1, 'd': 1}, {'a': 2, 'b': 1, 'c': 3, 'd': 4}])
+    test = pd.concat([df1, df2])
+
+    return test
+
